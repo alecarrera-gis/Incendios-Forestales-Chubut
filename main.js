334cef6326cb2b34c8fb68a207492cd5aa3ef81d
Alejandro Carrera <alejandrodavidcarrera@gmail.com>
2026-01-21 18:25:20 -0300

    Sentinel 2

--- main.js
+++ main.js
@@ -12,8 +12,8 @@ document.addEventListener('DOMContentLoaded', () => {
     // Crear el mapa centrado en la zona de Chubut
     const map = L.map('map', {
         center: [-42.73, -71.69],
-        zoom: 14,
-        minZoom: 12,
+        zoom: 8,
+        minZoom: 8,
         maxZoom: 14
     });
 
@@ -32,28 +32,28 @@ document.addEventListener('DOMContentLoaded', () => {
     
     const sentinel2_20260119 = L.tileLayer('20260119/{z}/{x}/{y}.jpg', {
         maxZoom: 14,
-        minZoom: 12,
+        minZoom: 8,
         attribution: 'Sentinel-2 © Copernicus',
         tileSize: 256
     });
 
     const sentinel2_20260109 = L.tileLayer('20260109/{z}/{x}/{y}.jpg', {
         maxZoom: 14,
-        minZoom: 12,
+        minZoom: 8,
         attribution: 'Sentinel-2 © Copernicus',
         tileSize: 256
     });
 
     const sentinel2_20260104 = L.tileLayer('20260104/{z}/{x}/{y}.jpg', {
         maxZoom: 14,
-        minZoom: 12,
+        minZoom: 8,
         attribution: 'Sentinel-2 © Copernicus',
         tileSize: 256
     });
 
     const sentinel2_20251125 = L.tileLayer('20251125/{z}/{x}/{y}.jpg', {
         maxZoom: 14,
-        minZoom: 12,
+        minZoom: 8,
         attribution: 'Sentinel-2 © Copernicus',
         tileSize: 256
     });
